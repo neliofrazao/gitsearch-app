aed1f4e87b80ac7317fd763f5eda7c38b5254ddf
Nelio Frazao <neliofrazac@gmail.com>
2020-05-16 13:40:42 -0300

    Revert "test: 'should title attribute to equal a 'Some Link'"
    This reverts commit d7a7f4d19b75a392318c14c1a5d6de5c44e98a40.

--- src/Shared/NavLink/NavLink.test.js
+++ src/Shared/NavLink/NavLink.test.js
@@ -16,11 +16,4 @@ describe('NavLink()', () => {
 
     expect(SUT).toEqual('/topfive')
   })
-
-  test('should title attribute to equal a "Some Link"', () => {
-    const { getByTestId } = render(injectRouter(<BaseRender />))
-    const SUT = getByTestId('data-nav-link').getAttribute('title')
-
-    expect(SUT).toEqual('some link')
-  })
 })

e8a3737106acbe9388a0f8f3393f1dbe8649c492
Nelio Frazao <neliofrazac@gmail.com>
2020-05-16 13:51:34 -0300

    test: should returns renders children props

--- src/Shared/NavLink/NavLink.test.js
+++ src/Shared/NavLink/NavLink.test.js
@@ -5,7 +5,7 @@ import NavLink from './index'
 
 const BaseRender = () => (
   <NavLink to="/topfive" title="some link" data-testid="data-nav-link">
-    Some Link
+    <span data-testid="data-nav-children">Some Link</span>
   </NavLink>
 )
 
@@ -15,5 +15,19 @@ describe('NavLink()', () => {
     const SUT = getByTestId('data-nav-link').getAttribute('href')
 
     expect(SUT).toEqual('/topfive')
+  })
+
+  test('should title attribute retutns with "Some Link" values', () => {
+    const { getByTestId } = render(injectRouter(<BaseRender />))
+    const SUT = getByTestId('data-nav-link').getAttribute('title')
+
+    expect(SUT).toEqual('some link')
+  })
+
+  test('should returns renders children props', () => {
+    const { getByTestId } = render(injectRouter(<BaseRender />))
+    const SUT = getByTestId('data-nav-children').innerHTML
+
+    expect(SUT).toBe('Some Link')
   })
 })

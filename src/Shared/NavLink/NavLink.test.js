d7a7f4d19b75a392318c14c1a5d6de5c44e98a40
Nelio Frazao <neliofrazac@gmail.com>
2020-05-16 13:03:38 -0300

    test: 'should title attribute to equal a 'Some Link'

--- src/Shared/NavLink/NavLink.test.js
+++ src/Shared/NavLink/NavLink.test.js
@@ -16,4 +16,11 @@ describe('NavLink()', () => {
 
     expect(SUT).toEqual('/topfive')
   })
+
+  test('should title attribute to equal a "Some Link"', () => {
+    const { getByTestId } = render(injectRouter(<BaseRender />))
+    const SUT = getByTestId('data-nav-link').getAttribute('title')
+
+    expect(SUT).toEqual('some link')
+  })
 })

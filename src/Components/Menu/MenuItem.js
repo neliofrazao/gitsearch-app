252c4c5ffa36ce3175b9f99447325c74d35a0191
Nelio Frazao <neliofrazac@gmail.com>
2020-05-16 23:03:04 -0300

    feat: create MenuItem component

---
+++ src/Components/Menu/MenuItem.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Button, ListItem } from '@material-ui/core'
+import { NavLink } from '../../Shared'
+
+const MenuItem = ({ children, link, title, ...props }) => (
+  <ListItem {...props}>
+    <Button component={NavLink} to={link} title={title}>
+      {children}
+    </Button>
+  </ListItem>
+)
+
+MenuItem.propTypes = {
+  link: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  children: PropTypes.string.isRequired,
+}
+
+MenuItem.defaultProps = {
+  title: '',
+}
+
+export default MenuItem

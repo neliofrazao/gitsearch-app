9fe19e96090d58c0f00e7ca7bdf938dfe7b25951
Nelio Frazao <neliofrazac@gmail.com>
2020-05-17 19:24:14 -0300

    test: should GET user repos url with right param

--- src/api/users/users.test.js
+++ src/api/users/users.test.js
@@ -15,4 +15,18 @@ describe('Users()', () => {
     expect(result.url).toBe('/users/username')
     http.get.mockRestore()
   })
+
+  test('should GET user repos url with right param', async () => {
+    expect.assertions(1)
+    jest.spyOn(http, 'get')
+    http.get.mockImplementation((url) => ({
+      data: {
+        url,
+      },
+    }))
+
+    const result = await api.getUserRepo('username')
+    expect(result.url).toBe('/users/username/repos')
+    http.get.mockRestore()
+  })
 })

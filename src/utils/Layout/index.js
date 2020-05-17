567628c3e736a17c7d8e6f5ccd508dc53fd26859
Nelio Frazao <neliofrazac@gmail.com>
2020-05-16 23:21:32 -0300

    feat: add Layout component

---
+++ src/utils/Layout/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Header from '../../Components/Header'
+import Main from './Main'
+import GlobalStyle from './Layout.styles'
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <div data-testid="data-layout">
+        <Header />
+        <Main>{children}</Main>
+      </div>
+      <GlobalStyle />
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.element.isRequired,
+}
+
+export default Layout

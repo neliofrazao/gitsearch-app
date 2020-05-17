3fc5aeb5196f37d8da30dc600d72f5fd8bb20393
Nelio Frazao <neliofrazac@gmail.com>
2020-05-17 19:21:26 -0300

    test: should GET users url with right param

---
+++ src/api/users/users.test.js
@@ -0,0 +1,18 @@
+import http from '../../utils/http'
+import api from './users'
+
+describe('Users()', () => {
+  test('should GET users url with right param', async () => {
+    expect.assertions(1)
+    jest.spyOn(http, 'get')
+    http.get.mockImplementation((url) => ({
+      data: {
+        url,
+      },
+    }))
+
+    const result = await api.getUsers('username')
+    expect(result.url).toBe('/users/username')
+    http.get.mockRestore()
+  })
+})

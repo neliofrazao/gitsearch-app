0724f74944457cd9b72806cdd3fc23f63114fbf3
Nelio Frazao <neliofrazac@gmail.com>
2020-05-17 18:10:26 -0300

    feat: implement SearchForm in SearchUser and create validade schema

--- src/Pages/SearchUser/index.js
+++ src/Pages/SearchUser/index.js
@@ -1,5 +1,34 @@
 import React from 'react'
+import * as Yup from 'yup'
+import { Typography } from '@material-ui/core'
+import { SearchForm } from '../../Components'
 
-const SearchUser = () => <div data-testid="data-search-user">SearchUser</div>
+const REQUIRED_MESSAGE = 'Campo obrigatório'
+const schema = Yup.object().shape({
+  userName: Yup.string().required(REQUIRED_MESSAGE),
+})
+
+const initialValues = {
+  userName: '',
+}
+
+const handleSeachUser = async (payload) => {
+  console.log(payload)
+}
+
+const SearchUser = () => {
+  return (
+    <div data-testid="data-search-user">
+      <Typography variant="h4" component="h2" gutterBottom>
+        Busca por usuário do Github
+      </Typography>
+      <SearchForm
+        initialValues={initialValues}
+        formSchema={schema}
+        handleSubmit={handleSeachUser}
+      />
+    </div>
+  )
+}
 
 export default SearchUser

1ecc17b4dd93482eb034669dc377e3fa74ae906b
Nelio Frazao <neliofrazac@gmail.com>
2020-05-17 12:15:59 -0300

    feat: provide Load value in routes components

--- src/Routes.js
+++ src/Routes.js
@@ -1,23 +1,29 @@
-import React, { Suspense, lazy } from 'react'
+import React, { useState, useMemo, Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Layout from './utils/Layout'
 import RouteLoader from './utils/RouteLoader'
+import LoadContext from './Shared/Loading/store'
 
 const SearchUser = lazy(() => import('./Pages/SearchUser'))
 const SearchHistory = lazy(() => import('./Pages/SearchHistory'))
 const UserRank = lazy(() => import('./Pages/UserRank'))
 
 const Routes = () => {
+  const [isLoad, setIsLoad] = useState(false)
+  const provideLoad = useMemo(() => ({ isLoad, setIsLoad }), [isLoad, setIsLoad])
+
   return (
     <Router>
       <Suspense fallback={<RouteLoader />}>
-        <Layout>
-          <Switch>
-            <Route path="/" exact component={SearchUser} />
-            <Route path="/historico-busca" exact component={SearchHistory} />
-            <Route path="/rank" exact component={UserRank} />
-          </Switch>
-        </Layout>
+        <LoadContext.Provider value={provideLoad}>
+          <Layout>
+            <Switch>
+              <Route path="/" exact component={SearchUser} />
+              <Route path="/historico-busca" exact component={SearchHistory} />
+              <Route path="/rank" exact component={UserRank} />
+            </Switch>
+          </Layout>
+        </LoadContext.Provider>
       </Suspense>
     </Router>
   )

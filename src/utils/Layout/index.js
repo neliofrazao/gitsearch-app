6872e1128b19ec9df7b1f6add63d7f52223b9378
Nelio Frazao <neliofrazac@gmail.com>
2020-05-17 12:23:43 -0300

    feat: add loading state in Layout component, update type checking in Main component

--- src/utils/Layout/index.js
+++ src/utils/Layout/index.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import Header from '../../Components/Header'
 import Main from './Main'
+import LoadContext from '../../Shared/Loading/store'
+import Loading from '../../Shared/Loading'
 import GlobalStyle from './Layout.styles'
 
 const Layout = ({ children }) => {
+  const { isLoad } = useContext(LoadContext)
   return (
     <>
       <div data-testid="data-layout">
         <Header />
-        <Main>{children}</Main>
+        <Main>
+          {isLoad && <Loading />}
+          {children}
+        </Main>
       </div>
       <GlobalStyle />
     </>

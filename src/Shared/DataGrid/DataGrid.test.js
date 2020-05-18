4ce13230d15a2948ca0055847a3f05d6b9a5399a
Nelio Frazao <neliofrazac@gmail.com>
2020-05-18 11:37:38 -0300

    test: should mounts categories list

---
+++ src/Shared/DataGrid/DataGrid.test.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import DataGrid from './index'
+
+const columns = [
+  {
+    id: 'name',
+    label: 'Name',
+  },
+  {
+    id: 'description',
+    label: 'Description:',
+  },
+]
+
+const rows = [
+  {
+    id: 1,
+    name: 'User 1',
+    description: 'Description',
+  },
+  {
+    id: 2,
+    name: 'User 2',
+    description: 'Description',
+  },
+]
+
+describe('DataGrid()', () => {
+  test('should mounts categories list', () => {
+    const { container } = render(<DataGrid columns={columns} rows={rows} />)
+    expect(container).toBeDefined()
+  })
+})
